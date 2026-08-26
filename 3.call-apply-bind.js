f185ad1c739d88050efc5efc9225a18093cf9e7c
asm-saim <saim777m@gmail.com>
2026-08-26 19:16:20 +0600

    bnd method implemented

--- 3.call-apply-bind.js
+++ 3.call-apply-bind.js
@@ -10,6 +10,10 @@ const marks = {
   chemistry: 78,
 };
 
-//for instant calling:
-avg.call(marks, 45, 79);
-avg.apply(marks, [34, 67]);
+//for instant calling: call, apply
+avg.call(marks, 45, 79); //marks holo this er value
+avg.apply(marks, [34, 67]); //marks holo this er value
+
+//bind method: akon bind kore raklam, pore proyojon onujai call korlam.
+const bindCreating = avg.bind(marks);
+bindCreating(78, 32);
