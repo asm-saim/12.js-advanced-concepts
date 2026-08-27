8c88c2bd9cc93db61dbef839c66436fef8c52bae
asm-saim <saim777m@gmail.com>
2026-08-27 18:58:45 +0600

    nested callbanck implemented

--- 6.callback-function.js
+++ 6.callback-function.js
@@ -10,3 +10,38 @@ function callBackSum(something) {
   console.log(something);
 }
 sum(10, 10, callBackSum);
+
+//real life example:
+
+function getQuizMark(mark, cb) {
+  setTimeout(() => {
+    console.log("Quiz mark fetched!");
+    cb(mark);
+  }, 1000);
+}
+
+function getContestMark(mark, cb) {
+  setTimeout(() => {
+    console.log("Contest mark fetched");
+    cb(mark);
+  }, 2000);
+}
+function getInterviewMark(mark, cb) {
+  setTimeout(() => {
+    console.log("Interview mark fetched");
+    cb(mark);
+  }, 3000);
+}
+
+function totalMark(quizMark, contestMark, interviewMark) {
+  const total = quizMark + contestMark + interviewMark;
+  console.log(total);
+}
+
+getQuizMark(10, (quizMark) => {
+  getContestMark(415, (contestMark) => {
+    getInterviewMark(100, (interviewMark) => {
+      totalMark(quizMark, contestMark, interviewMark);
+    });
+  });
+});
