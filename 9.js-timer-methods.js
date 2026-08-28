4d68ab19bb34d929b8a739747f51b86de055819b
asm-saim <saim777m@gmail.com>
2026-08-28 15:23:47 +0600

    setinterval implemented

--- 9.js-timer-methods.js
+++ 9.js-timer-methods.js
@@ -1,6 +1,21 @@
 //JS default timer methods: setTimeOut, setInterval:
 
-// syntax: setTimeOut(fn, delay)
+// syntax: setTimeOut(fn, delay)-> one time execution
 setTimeout(() => {
   console.log("Hello");
 }, 5000);
+console.log("A");
+
+//How to withdraw setTimeOut:
+// const clear = setTimeout(() => {
+//   console.log("Greetings");
+// });
+// clearTimeout(clear);
+
+//setInterval: same task , repetitively doing. -> Multiple times execution.
+
+let count = 0;
+setInterval(() => {
+  count++;
+  console.log(`Value executed ${count} times.`);
+}, 1000);
