e6d46a926b953898f06abb4d3462dfa6796a294f
asm-saim <saim777m@gmail.com>
2026-08-28 13:23:09 +0600

    settimeout method implemented

--- 8.async-await.js
+++ 8.async-await.js
@@ -1,4 +1,11 @@
-//async , await make the Promise object more easier.
+//Promise with then , catch:
+function withThenCatch() {
+  const res = fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => console.log(res))
+    .catch((e) => console.log(e));
+}
+
+//Promise with async , await. It makes the Promise object more easier.
 async function titleCatcher() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
---
+++ 9.js-timer-methods.js
@@ -0,0 +1,6 @@
+//JS default timer methods: setTimeOut, setInterval:
+
+// syntax: setTimeOut(fn, delay)
+setTimeout(() => {
+  console.log("Hello");
+}, 5000);
